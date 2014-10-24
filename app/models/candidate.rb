dd4e720409f1728cc56fc3c1ab9409c9dd4a2dfd
liviu <unknownliviu@gmail.com>
2014-10-25 00:14:32 +0300

    added annotations

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: candidates
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  sex        :string(255)
+#  university :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Candidate < ActiveRecord::Base
+  acts_as_votable
 end
